8c66f6fcd77ef3c7933c4b6ee0852684514ace18
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-31 21:35:08 +0000

    added note component and styled it

--- src/js/component/note.jsx
+++ src/js/component/note.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./task";
 
 const Note = () => {
+    const [inputValue, setInputValue ] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter" && inputValue.trim() !== "") {
+            const newTask = {
+                text: inputValue.trim(),
+                id: tasks.length + 1
+            }
+
+            setTasks([newTask, ...tasks]);
+
+            setInputValue(""); 
+        }
+    };
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
 	return (
 		<div className="col-lg-4  col-sm-11 col-md-8 d-flex flex-column justify-content-center align-items-center">
-			<div className="note align-middle p-2 w-100">
-				<div>0 items left.</div>
+			<div className="note align-middle w-100 outline">
+				<div className="py-2 px-5 border-bottom">
+                    <input 
+                        type="text" 
+                        className="form-control border-0" 
+                        id="input-text" 
+                        placeholder="What needs to be done?" 
+                        onChange={handleChange}
+                        onKeyDown={handleKeyDown} 
+                        value={inputValue}
+                    />
+                </div>
+
+                {tasks.map((task) => (
+                    <React.Fragment key={task.id}>
+                        <Task task={task.text} border={'border-bottom'} />
+                    </React.Fragment>
+                ))}
+
+				<div className="p-2" style={{ fontSize: '0.8rem' }}>{tasks.length == 0 ? 'No tasks. Add a task.' : `${tasks.length} items left.`}</div>
 			</div>
-			<div className="note" style={{ width: `95%`, height: '5px', zIndex: '-1' }}></div>
-			<div className="note" style={{ width: `90%`, height: '5px', zIndex: '-2'}}></div>
+			<div className="note outline" style={{ width: `95%`, height: '5px', zIndex: '-1' }}></div>
+			<div className="note outline" style={{ width: `90%`, height: '5px', zIndex: '-2'}}></div>
 		</div>
 	);
 };
