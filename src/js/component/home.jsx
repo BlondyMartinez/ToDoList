02ed42756925134577fe94b68c6b840102bb56bf
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-01 12:37:29 +0000

    added themes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState }  from "react";
 import Note from "./note"
 
 //create your first component
 const Home = () => {
+	const [theme, setTheme] = useState('light-theme');
+
+	const handleClick = () => {
+		setTheme(theme === 'light-theme' ? 'dark-theme' : 'light-theme');
+    };
+
 	return (
-		<div className="d-flex flex-column justify-content-center align-items-center">
+		<div className={`d-flex flex-column align-items-center bg ${theme}`}>
+			<div onClick={handleClick} className="ms-auto m-2" role="button"> 
+				{theme == 'light-theme' ? <i className="fa-solid fa-moon"></i> : <i className="fa-solid fa-sun"></i>} 
+			</div>
 			<h1 className="text-center mt-5">todos</h1>
 			<Note />
 		</div>
