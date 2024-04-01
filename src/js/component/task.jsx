0f4cb6180ceac7079a6097b959b2296e55b6f466
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-01 12:38:05 +0000

    removed redundant functions

--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -3,23 +3,20 @@ import React, { useState } from "react";
 const Task = (props) => {
     const [onHover, setOnHover] = useState(false);
 
-    const handleMouseEnter = () => {
-        setOnHover(true);
-    };
-
-    const handleMouseLeave = () => {
-        setOnHover(false);
-    };
-
     const removeThis = () => {
         const updatedTasks = props.tasks.filter(task => task.id !== props.id);
         props.setTasks(updatedTasks);
     };
 
 	return (
-        <div className="py-2 px-5 border-bottom d-flex justify-content-between" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div 
+        className="py-2 px-5 border-at-bottom d-flex justify-content-between" 
+            onMouseEnter={() => { setOnHover(true) }} 
+            onMouseOver={() => { setOnHover(true) }} 
+            onMouseLeave={() => { setOnHover(false) }}
+        >
             <div>{props.task}</div>
-            {onHover && <div onClick={removeThis}><i className="text-danger fa-solid fa-xmark"></i></div>}
+            {onHover && <div onClick={removeThis} role="button"><i className="text-danger fa-solid fa-xmark"></i></div>}
         </div>
 	);
 };
